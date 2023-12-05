d2cc4d2b2dfaf0e55e020379b49777f6ea18c63a
A253560 <A253560@LSBSA-CCTCSV3>
2023-12-05 14:07:57 +0200

    Add project files.

---
+++ The for loop/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_for_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The while loop for reference");
+
+            int i = 0;
+
+            while (i <= 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            Console.WriteLine("This a for loop");
+
+            for (i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
